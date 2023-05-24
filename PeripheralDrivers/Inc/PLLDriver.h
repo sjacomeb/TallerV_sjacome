556d3875d1935ead483f4d5ff8ac055921a298b0
sjacomeb <sjacome@unal.edu.co>
2023-05-24 06:55:59 -0500

    Ultima actualizacion PLL

--- PeripheralDrivers/Inc/PLLDriver.h
+++ PeripheralDrivers/Inc/PLLDriver.h
@@ -10,11 +10,10 @@
 
 #include <stm32f4xx.h>
 
-#define HSI_VALUE   10000000	//10MHz
-#define HSE_VALUE	80000000    //80 MHz
+#define HSI_VALUE   16	//16MHz
 
 void configPLL(void);
-uint32_t getConfigPLL(uint32_t freqClock);
+uint8_t getConfigPLL(void);
 
 
 #endif /* PLLDRIVER_H_ */
--- PeripheralDrivers/Src/PLLDriver.c
+++ PeripheralDrivers/Src/PLLDriver.c
@@ -11,22 +11,9 @@
 //Función para cargar la configuración de los registros
 void configPLL(void){
 
-	//Activamos la señal de reloj del Power Control
-	RCC->APB1ENR |= RCC_APB1ENR_PWREN;
-
-//	//Para trabajar con velocidades >= 84 MHz y <= 100MHz
-//	PWR->CR &= ~(PWR_CR_VOS);
-//	PWR->CR |= (PWR_CR_VOS);
-
 	//Wait states to the Latency
-	FLASH->ACR |= FLASH_ACR_ICEN | FLASH_ACR_DCEN | FLASH_ACR_PRFTEN ;
 	FLASH->ACR  &= ~(FLASH_ACR_LATENCY);
 	FLASH->ACR |= (FLASH_ACR_LATENCY_2WS); //64 < HCLK ≤ 90
-
-	/* Modify the CPU clock */
-	//SW : System clock switch
-	RCC -> CFGR &= ~(RCC_CFGR_SW);
-	RCC->CFGR |= RCC_CFGR_SW_1;   // PLL selected as system clock
 
 	RCC->PLLCFGR &= ~(RCC_PLLCFGR_PLLSRC); // HSI clock selected as PLL clock entry
 
@@ -44,7 +31,7 @@ void configPLL(void){
 	//f(Clock output) = f(VCO clock) / PLLP
 	//La frecuencia deseada es 80 MHz, entonces PLLP = 6 MHz
 	RCC->PLLCFGR &= ~(RCC_PLLCFGR_PLLP);
-	RCC->PLLCFGR |= (RCC_PLLCFGR_PLLP_0);
+	RCC->PLLCFGR |= (RCC_PLLCFGR_PLLP_1);
 
 	/* División de los buses */
 	//AHB prescaler, no se divide (trabaja a 100MHz > 80 MHz)
@@ -62,12 +49,16 @@ void configPLL(void){
 	// Seleccionamos la señal PLL
 	RCC -> CFGR |= RCC_CFGR_MCO1;
 
-	//Division de 5 (preescaler)
-	RCC ->CFGR &= RCC_CFGR_MCO1PRE;
-	RCC -> CFGR |= RCC_CFGR_MCO1_1;
+	//Division de 4 (preescaler)
+
+	RCC -> CFGR |= (RCC_CFGR_MCO1PRE);
+	RCC ->CFGR &= ~(RCC_CFGR_MCO1PRE_0);
 
 	/*Se enciende el PLL*/
 	RCC->CR |= RCC_CR_PLLON;
+
+	//SW : System clock switch
+	RCC->CFGR |= RCC_CFGR_SW_1;  // PLL selected as system clock
 
 	// Esperamos que el PLL se cierre (estabilizacion)
 	while (!(RCC->CR & RCC_CR_PLLRDY)){
@@ -77,12 +68,12 @@ void configPLL(void){
 }
 
 //Función que entrega la frecuencia
-uint32_t getConfigPLL(uint32_t freqClock){
+uint8_t getConfigPLL(void){
 
-	freqClock = 0;
+	uint8_t freqClock = 0;
 
-	uint32_t valuePlln = ((RCC_PLLCFGR_PLLN & RCC_PLLCFGR_PLLN_Msk) >> RCC_PLLCFGR_PLLN_Pos);
-	uint32_t valuePllm = ((RCC_PLLCFGR_PLLM & RCC_PLLCFGR_PLLM_Msk) >> RCC_PLLCFGR_PLLM_Pos);
+	uint16_t valuePlln = ((RCC_PLLCFGR_PLLN & RCC_PLLCFGR_PLLN_Msk) >> RCC_PLLCFGR_PLLN_Pos);
+	uint16_t valuePllm = ((RCC_PLLCFGR_PLLM & RCC_PLLCFGR_PLLM_Msk) >> RCC_PLLCFGR_PLLM_Pos);
 
 	if( freqClock == 16 ){
 
@@ -91,7 +82,7 @@ uint32_t getConfigPLL(uint32_t freqClock){
 	}
 	else{
 
-		freqClock = ((HSE_VALUE * valuePlln) / valuePllm) / 2 ;		//valuePLLP = 2
+		freqClock = ((HSI_VALUE * valuePlln) / valuePllm) / 2 ;		//valuePLLP = 2
 	}
 
 	return freqClock ;
